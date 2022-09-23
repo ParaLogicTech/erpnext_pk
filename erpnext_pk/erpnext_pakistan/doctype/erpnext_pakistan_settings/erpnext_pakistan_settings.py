c11dd691583285804776971f3ccc0daf16549035
Saif Ur Rehman <saifi0102@gmail.com>
2022-09-23 17:32:26 +0500

    Revert "chore: Remove empty ERPNext Pakistan Settings DocType"
    This reverts commit d0c06dcacbe642da8806998e2d56b458e0f529c5.

---
+++ erpnext_pk/erpnext_pakistan/doctype/erpnext_pakistan_settings/erpnext_pakistan_settings.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, ParaLogic and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class ERPNextPakistanSettings(Document):
+	pass
